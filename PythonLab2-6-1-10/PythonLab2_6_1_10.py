18f0fb5acdef1544534deb219f02a4d2f836d5c5
IR Harvey <ir_harvey101@hotmail.co.uk>
2022-07-27 11:57:04 +0100

    Add project files.

---
+++ PythonLab2-6-1-10/PythonLab2_6_1_10.py
@@ -0,0 +1,30 @@
+#Oridginal
+#x = float(input("Enter value for x: "))
+# Write your code here.
+#print("y =", y)
+
+#Scenario
+#User inputs an number
+#convert to 'float'
+x = float(input("Enter value for x: "))
+#Put 'float' through a calculation
+y = 1/(x + (1/(x + (1/(x + (1/x))))))
+print("y =", y)
+
+#Extra
+#a = 1 / x
+#b = x + a 
+#c = 1 / b
+#d = x + c 
+#e = 1 / d
+#f = x + e
+#g = 1 / f
+#
+#print("\nHello, this is a breakdown of the calculation:")
+#print("a =", a)
+#print("b =", b)
+#print("c =", c)
+#print("d =", d)
+#print("e =", e)
+#print("f =", f)
+#print("\nWith your final answer being:\n", g, end="\n\n")
